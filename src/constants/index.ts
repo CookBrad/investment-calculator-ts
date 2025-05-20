9293e017b69342ce41adb9ed8964b6500aee06aa
Brad Cook <cook.brada@gmail.com>
2025-05-20 12:20:55 -0500

    test different apis

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,8 @@
-const BASE_URL = 'https://query1.finance.yahoo.com/v8/finance/chart/';
+const ALPHA_BASE_URL = 'https://query2.finance.yahoo.com/v8/finance/chart/';
+const FIN_BASE_URL = 'https://finnhub.io/api/v1/quote';
+const POLYGON_BASE_URL = 'https://api.polygon.io/v2/aggs/ticker/';
 export {
-    BASE_URL,
+    ALPHA_BASE_URL,
+    FIN_BASE_URL,
+    POLYGON_BASE_URL
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import get from 'axios';
-import { BASE_URL } from './constants';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { POLYGON_BASE_URL } from './constants';
 import { ChartResult } from './types';
 
 
@@ -10,8 +12,16 @@ async function main(): Promise<void> {
 }
 
 async function getStockData(symbol: string): Promise<ChartResult> {
-  const url: string = `${BASE_URL}${symbol}`;
-  const response = await get(url);
+  const period1 = new Date('2025-05-06T00:00:00Z').getTime() / 1000;
+  const period2 = new Date('2025-05-07T23:59:59Z').getTime() / 1000;
+  const interval = '1d';
+  const params = {
+
+    apiKey: process.env.POLYGON_API_KEY
+  }
+  const url: string = `${POLYGON_BASE_URL}${symbol}/range/1/day/2025-05-07/2025-05-07`;
+  const response = await get(url, { params });
+  console.log(JSON.stringify(response.data, null, 2))
   return response.data.chart.result[0];
 }
 
